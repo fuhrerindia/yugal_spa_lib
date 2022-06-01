44e9446703f38ee34dd2d5c4c2922e0151a4c6c7
Paurush Sinha <76876045+sinhapaurush@users.noreply.github.com>
2022-06-01 16:34:56 +0530

    Update header.php

--- header.php
+++ header.php
@@ -2,13 +2,17 @@
     function spa_root(){
         return '
             <div id="yugal-root"></div>
+            <style id="yugal-style"></style>
         ';
     }
     function spa_preloader($preloader){
         return '
             <div id="yugal-root">'.$preloader.'</div>
+            <style id="yugal-style"></style>
         ';
     }
+    function create_spa(){
+        echo '<script src="./lib/yugal_spa_lib/yugal.js"></script>';
+    }
 ?>
-<script src="./lib/yugal_spa/yugal.js"></script>
-<link rel="stylesheet" href="./lib/yugal_spa/yugal.css">
+<link rel="stylesheet" href="./lib/yugal_spa_lib/yugal.css">
