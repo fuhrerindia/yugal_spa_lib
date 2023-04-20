b8c02fc361666d4d089c784188fa9aeee80fd804
Paurush Sinha <76876045+sinhapaurush@users.noreply.github.com>
2023-04-20 19:07:59 +0530

    Update yugal.js

--- yugal.js
+++ yugal.js
@@ -1,260 +1,251 @@
-function YugalErrorPage(title, error) {
-    return {
-        willMount: () => {
-            console.error(error);
-        },
-        render: `
-        <style>
-            article{
-                width:100vw;
-                height: 100vh;
-                background: red;
-                text-align: center;
-                overflow-y:auto;
-            }
-            section{
-                width: 60%;
-                height: 100vh;
-                background: #000;
-                display: inline-block; 
-                text-align:left;
-            }
-            h1{
-                margin: 50px;
-                color: #ffffff;
-            }
-            div{
-                width:100%;
-                text-align:center;
-            }
-            pre{
-                width: 90%;
-                background: #404040;
-                color:#fff;
-                border-radius: 10px;
-                padding: 10px;
-                display:inline-block;
-                line-height:25px;
-                text-align:left;
-            }
-            button{
-                padding:10px;
-                border-radius: 10px;
-                margin:10px;
-                border:0;
-                cursor:pointer;
-                background:#34cceb;
-                color:#000;
-                transition:0.3s;
-            }
-            button:hover{
-                background: #087f96;
-                color:#fff
-            }
-        </style>
-            <article>
-                <section>
-                    <h1>${title.toUpperCase()} ERROR</h1>
-                    <div>
-                    <pre>${error}</pre>
-                    <br>
-                    <a href=""><button>RELOAD</button></a>
-                    <a href="https://spa.yugal.ml" target="_blank"><button>DOCS</button></a>
-                    </div>
-                </section>
-            </article>
-        `
-    };
-}
 const YugalVars = {
-    root: document.getElementById("yugal-root"),
-    storageKey: "yugal-page-navigation-history",
-    yugalHeadComment: `<!-- DO NOT DELETE THIS COMMENT! THIS COMMENT IS VERY IMPORTANT FOR YUGAL TO WORK! -->`,
-    yugalErr: (error, type) => {
-        yugal.navigate(YugalErrorPage(error, type));
-    },
-    pageNum: 1,
-    importedCss: 0
+  root: document.getElementById("yugal-root"),
+  yugalHeadComment: `<!-- DO NOT DELETE THIS COMMENT! THIS COMMENT IS VERY IMPORTANT FOR YUGAL TO WORK! -->`,
+  pageNum: 1,
+  importedCss: 0,
 };
+var page = {};
+function navigate_to_path({
+  willMount,
+  onMount,
+  render,
+  uri,
+  historyTitle,
+  css,
+  willUnMount,
+  onUnMount,
+}) {
+  page = {};
+  yugal.currentDestroy();
 
+  if (willUnMount !== undefined) {
+    yugal.currentDestroy = willUnMount;
+  } else {
+    yugal.currentDestroy = () => {};
+  }
+  if (css !== undefined) {
+    document.getElementById("yugal-page-specific-style").innerHTML = css;
+  } else {
+    document.getElementById("yugal-page-specific-style").innerHTML = "";
+  }
+  historyTitle = !historyTitle ? uri : historyTitle;
+  if (willMount !== undefined) {
+    willMount();
+  }
+  if (render !== undefined) {
+    if ((YugalVars.root.innerHTML = render)) {
+      if (onMount !== undefined) {
+        onMount();
+      }
+    } else {
+      console.error("COMPILATION ERROR");
+    }
+  } else {
+    console.error("COMPILATION ERROR");
+  }
+  if (uri !== undefined) {
+    history.pushState({ urlPath: `./${uri}` }, historyTitle, `./${uri}`);
+    YugalVars.pageNum++;
+  }
+  const elements = document.querySelectorAll("[to]");
+  elements.forEach((element) => {
+    let toValue = element.getAttribute("to");
+    if (element.getAttribute("onclick") !== null) {
+      toValue_past = element.getAttribute("onclick");
+    } else {
+      toValue_past = "";
+    }
+    if (toValue_past.replaceAll(" ") === "") {
+      toValue = `yugal.link("${toValue}");`;
+    } else {
+      toValue = `${toValue_past};yugal.link("${toValue}");`;
+    }
+    toValue = toValue.replaceAll(";;", ";");
+    element.setAttribute("onclick", toValue);
+    element.removeAttribute("to");
+  });
+  yugal.afterDestroyed();
+  if (onUnMount !== undefined) {
+    yugal.afterDestroyed = onUnMount;
+  } else {
+    yugal.afterDestroyed = () => {};
+  }
+}
 const yugal = {
-    production: () => {
-        console.log('%cThis is a browser feature intended for developers. Do not enter anything here.', "background:black ;color: white; font-size: x-large");
-        console = {
-            log: () => { },
-            error: () => { },
-            warn: () => { }
-        };
-        eval = () => { }
-        YugalErrorPage = () => { return {} }
-    },
-    navigate: ({ willMount, onMount, render, uri, historyTitle }) => {
-        historyTitle = !historyTitle ? uri : historyTitle;
-        if (willMount !== undefined) {
-            willMount();
-        }
-        if (render !== undefined) {
-            if (YugalVars.root.innerHTML = render) {
-                if (onMount !== undefined) {
-                    onMount();
-                }
-            } else {
-                yugal.navigate(YugalErrorPage("COMPILATION", `Could not Render component to DOM, You likely have not defined render component properly, check string-laterals. Skipped onMount LifeCycle method.`));
-
-            }
-        } else {
-            yugal.navigate(YugalErrorPage("COMPILATION", `Could not find render method in component object.
-Example Screen:
-function SomePage(){
-    return{
-        onMount: ()=>{}, //LifeCycle Method
-        willMount: ()=>{}, //LifeCycle Method
-        render: '', //Body Code to render to body
-        uri: '' //URI to push into browser
+  production: () => {
+    console.log(
+      "%cThis is a browser feature intended for developers. Do not enter anything here.",
+      "background:black ;color: white; font-size: x-large"
+    );
+    console = {
+      log: () => {},
+      error: () => {},
+      warn: () => {},
     };
-}`));
-        }
-        if (uri !== undefined) {
-            // history.pushState({ page: YugalVars.pageNum }, uri, uri);
-            history.pushState({urlPath:`./${uri}`}, historyTitle, `./${uri}`)
-            YugalVars.pageNum++;
-        }
-    },
-    title: (title) => {
-        if (title !== undefined) {
-            document.getElementsByTagName("title")[0].innerHTML = title;
-            document.getElementsByTagName("meta").title.content = title;
-            return true;
-        } else {
-            return document.getElementsByTagName("title")[0].innerHTML;
-        }
-    },
-    meta: (title, content) => {
-        if (document.getElementsByTagName("meta")[title] !== undefined) {
-            document.getElementsByTagName("meta")[title].content = content;
-        } else {
-            let headTag = document.getElementsByTagName("head")[0].innerHTML;
-            let siteCredential = headTag.split(YugalVars.yugalHeadComment)[0];
-            siteCredential = `
-                ${siteCredential}
-                <meta name="${title}" content="${content}" />
-            `
-            document.getElementsByTagName("head")[0].innerHTML = `${siteCredential}${YugalVars.yugalHeadComment}${headTag.split(YugalVars.yugalHeadComment)[1]}`;
-        }
-    },
-    header: (code) => {
-        if (code == undefined) {
-            YugalVars.yugalErr('SYNTAX', 'Can not find code to render in yugal.header method, you have to pass code as strings as parameter in yugal.header');
-        } else {
-            let headCodeSplit = document.getElementsByTagName("head")[0].innerHTML.split(YugalVars.yugalHeadComment);
-            document.getElementsByTagName("head")[0].innerHTML = `${headCodeSplit[0]}${YugalVars.yugalHeadComment}${code}`;
-        }
-    },
-    router: ({ initial, error404, screens }) => {
-        function dothis() {
-            let init = initial == undefined ? screens[0].page : initial;
-            const uri = window.location.href;
-            let req = uri.split("/");
-            req = req[req.length - 1];
-            if (req === "") {
-                yugal.navigate(init());
-            } else {
-                const svar = screens.find(
-                    (scrn) => {
-                        return scrn().uri === req
-                    }
-                );
-                if (svar == undefined) {
-                    if (error404 == undefined) {
-                        yugal.navigate(YugalErrorPage('404', `
-                            HTTP Error 404, Page could not be found.
-                        `));
-                    } else {
-                        yugal.navigate(error404());
-                    }
-                } else {
-                    yugal.navigate(svar());
-                }
-            }
-        }
-        dothis();
-        window.onpopstate = function (event) {
-            dothis();
-        }
-    },
-    include: (file) => {
-        var script = document.createElement('script');
-        script.src = file;
-        script.type = 'text/javascript';
-        document.getElementsByTagName('body').item(0).appendChild(script);
-    },
-    call: (file) => {
-        var script = document.createElement('script');
-        script.src = `./modules/${file}/index.js`;
-        script.type = 'text/javascript';
-        document.getElementsByTagName('body').item(0).appendChild(script);
-    },
-    files: (array) => {
-        array.map((item) => {
-            yugal.include(item);
+    eval = () => {};
+    YugalErrorPage = () => {
+      return {};
+    };
+  },
+  afterDestroyed: () => {},
+  navigate: (props) => {
+    navigate_to_path(props);
+  },
+  title: (title) => {
+    if (title !== undefined) {
+      document.getElementsByTagName("title")[0].innerHTML = title;
+      document.getElementsByTagName("meta").title.content = title;
+      return true;
+    } else {
+      return document.getElementsByTagName("title")[0].innerHTML;
+    }
+  },
+  meta: (title, content) => {
+    if (document.getElementsByTagName("meta")[title] !== undefined) {
+      document.getElementsByTagName("meta")[title].content = content;
+    } else {
+      let headTag = document.getElementsByTagName("head")[0].innerHTML;
+      let siteCredential = headTag.split(YugalVars.yugalHeadComment)[0];
+      siteCredential = `
+              ${siteCredential}
+              <meta name="${title}" content="${content}" />
+          `;
+      document.getElementsByTagName("head")[0].innerHTML = `${siteCredential}${
+        YugalVars.yugalHeadComment
+      }${headTag.split(YugalVars.yugalHeadComment)[1]}`;
+    }
+  },
+  header: (code) => {
+    let headCodeSplit = document
+      .getElementsByTagName("head")[0]
+      .innerHTML.split(YugalVars.yugalHeadComment);
+    document.getElementsByTagName(
+      "head"
+    )[0].innerHTML = `${headCodeSplit[0]}${YugalVars.yugalHeadComment}${code}`;
+  },
+  knownPath: {},
+  link: (pathname) => {
+    function VirtualPage() {
+      return yugal.knownPath[pathname]();
+    }
+    yugal.navigate(VirtualPage());
+  },
+  currentDestroy: () => {},
+  router: ({ initial, error404, screens }) => {
+    screens.forEach((i) => {
+      obj = i();
+      uri = obj.uri;
+      yugal.knownPath[`/${uri}`] = i;
+    });
+    function dothis() {
+      let init = initial == undefined ? screens[0].page : initial;
+      const uri = window.location.href;
+      let req = uri.split("/");
+      req = req[req.length - 1];
+      if (req === "") {
+        yugal.navigate(init());
+      } else {
+        const svar = screens.find((scrn) => {
+          return scrn().uri === req;
         });
-    },
-    kebabize: str => {
-        return str.split('').map((letter, idx) => {
-            return letter.toUpperCase() === letter
-                ? `${idx !== 0 ? '-' : ''}${letter.toLowerCase()}`
-                : letter;
-        }).join('');
-    },
-    style: (obj) => {
-        des = "";
-        Object.keys(obj).forEach(function (nkey) {
-            end = "";
-            if (typeof obj[nkey] === 'number'){
-                end = `${obj[nkey]}px;`;
-            }
-            else{
-                end = `${obj[nkey]};`
-            }
-            des = des + "" + yugal.kebabize(nkey) + ":" + end;
+        if (svar == undefined) {
+          if (error404 == undefined) {
+            console.error("PAGE NOT FOUND");
+          } else {
+            yugal.navigate(error404());
+          }
+        } else {
+          yugal.navigate(svar());
+        }
+      }
+    }
+    dothis();
+    window.onpopstate = function (event) {
+      dothis();
+    };
+  },
+  include: (file) => {
+    var script = document.createElement("script");
+    script.src = file;
+    script.type = "text/javascript";
+    document.getElementsByTagName("body").item(0).appendChild(script);
+  },
+  call: (file) => {
+    var script = document.createElement("script");
+    script.src = `./modules/${file}/index.js`;
+    script.type = "text/javascript";
+    document.getElementsByTagName("body").item(0).appendChild(script);
+  },
+  files: (array) => {
+    array.map((item) => {
+      yugal.include(item);
+    });
+  },
+  kebabize: (str) => {
+    return str
+      .split("")
+      .map((letter, idx) => {
+        return letter.toUpperCase() === letter
+          ? `${idx !== 0 ? "-" : ""}${letter.toLowerCase()}`
+          : letter;
+      })
+      .join("");
+  },
+  style: (obj) => {
+    des = "";
+    Object.keys(obj).forEach(function (nkey) {
+      end = "";
+      if (typeof obj[nkey] === "number") {
+        end = `${obj[nkey]}px;`;
+      } else {
+        end = `${obj[nkey]};`;
+      }
+      des = des + "" + yugal.kebabize(nkey) + ":" + end;
+    });
+    return des;
+  },
+  css: (props, name) => {
+    if (typeof props !== "string") {
+      props = yugal.style(props);
+    }
+    yugal_style = document.getElementById("yugal-style");
+    yugal_style.innerHTML = `${yugal_style.innerHTML}${name}{${props}}`;
+  },
+  $: (key) => document.querySelector(key),
+  StyleSheet: {
+    create: (css, beg) => {
+      beg = !beg ? "" : beg;
+      final_css = "";
+      Object.keys(css).map((key) => {
+        this_props = `${beg}${key}{`;
+        Object.keys(css[key]).map((prop) => {
+          prop_val =
+            typeof css[key][prop] === "number"
+              ? `${String(css[key][prop])}px`
+              : css[key][prop];
+          this_props = `${this_props}${yugal.kebabize(prop)}:${prop_val};`;
         });
-        return des;
+        this_props = this_props + `} `;
+        final_css = final_css + this_props;
+      });
+      return final_css;
     },
-    css: (props, name)=>{
-        if (typeof props !== 'string'){
-            props = yugal.style(props);
-        }
-        yugal_style = document.getElementById("yugal-style");
-        yugal_style.innerHTML = `${yugal_style.innerHTML}${name}{${props}}`
+    inject: (css_string) => {
+      document.getElementById("yugal-style").innerHTML = `${
+        document.getElementById("yugal-style").innerHTML
+      }${css_string}`;
     },
-    $: (key)=>document.querySelector(key),
-    StyleSheet: {
-        create: (css, beg) => {
-            beg = !beg ? "" : beg;
-            final_css = "";
-            Object.keys(css).map(key=>{
-                this_props = `${beg}${key}{`;
-                Object.keys(css[key]).map(prop=>{
-                    prop_val = typeof css[key][prop] === "number" ? `${String(css[key][prop])}px` : css[key][prop];
-                    this_props = `${this_props}${yugal.kebabize(prop)}:${prop_val};`;
-                });
-                this_props = this_props + `} `;
-                final_css = final_css + this_props;
-            });
-            return final_css;
-        },
-        inject: (css_string) => {
-            document.getElementById("yugal-style").innerHTML = `${document.getElementById("yugal-style").innerHTML}${css_string}`;
-        },
-        import: (url, id) => {
-            id = !id ? `yugal_imported_css${YugalVars.importedCss}` : id;
-            headcode = document.getElementsByTagName("head")[0].innerHTML;
-            headcode = headcode.split(YugalVars.yugalHeadComment);
-            headcode[0] = `<link rel="stylesheet" href="${url}" id="${id}">${headcode[0]}`;
-            document.getElementsByTagName("head")[0].innerHTML = `${headcode[0]}${YugalVars.yugalHeadComment}${headcode[1]}`;
-            YugalVars.importedCss = YugalVars.importedCss + 1;
-            return document.getElementById(id);
-        }
-    } 
+    import: (url, id) => {
+      id = !id ? `yugal_imported_css${YugalVars.importedCss}` : id;
+      headcode = document.getElementsByTagName("head")[0].innerHTML;
+      headcode = headcode.split(YugalVars.yugalHeadComment);
+      headcode[0] = `<link rel="stylesheet" href="${url}" id="${id}">${headcode[0]}`;
+      document.getElementsByTagName(
+        "head"
+      )[0].innerHTML = `${headcode[0]}${YugalVars.yugalHeadComment}${headcode[1]}`;
+      YugalVars.importedCss = YugalVars.importedCss + 1;
+      return document.getElementById(id);
+    },
+  },
 };
 const html = (code) => code;
